2e2df0217aecdfadecaf45a2d5c2c5141c709927
kcceelam <mail2ceelam@gmail.com>
2024-02-29 13:28:39 +0530

    commit for the first time

---
+++ src/test/java/basicscds/Basics.java
@@ -0,0 +1,9 @@
+package basicscds;
+
+public class Basics {
+	public static void main(String[] args) {
+		System.out.println("hii");
+		
+		
+	}
+}
---
+++ src/test/java/basicscds/Demo_1.java
@@ -0,0 +1,10 @@
+package basicscds;
+
+import org.testng.annotations.Test;
+
+public class Demo_1 {
+	@Test
+	public void add() {
+		System.out.println("Addition method");	}
+
+}
